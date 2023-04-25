607857aa95856a1e21a5b682acd78ce744a084b1
Frank Merlo <slytbizx_system@hotmail.com>
2023-04-25 18:31:31 -0300

    Nueva carpeta

---
+++ Ejercicio_1_funciones/funcion_suma.java
@@ -0,0 +1,16 @@
+package openBootcamp.Ejercicio_1_funciones;
+
+public class funcion_suma {
+    public static void main(String[] args) {
+
+        suma(10, 30, 40);
+    }
+
+    public static void suma(int a, int b, int c) {
+
+        int resultado;
+        resultado = a + b + c;
+
+        System.out.println(resultado);
+    }
+}
