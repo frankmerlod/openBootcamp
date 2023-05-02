baf6d7d846107f5b288d538d1f9eaf6e183fef2c
Frank Merlo <86177938+frankmerlod@users.noreply.github.com>
2023-05-02 02:50:55 -0300

    Update modulo_mathy.py

--- Python/modulo_mathy.py
+++ Python/modulo_mathy.py
@@ -1,5 +1,5 @@
 import mathy.mathy as mat
-# Modulo math ==> https://github.com/frankmerlod/openBootcamp/tree/main/Python/mathy
+# Modulo mathy ==> https://github.com/frankmerlod/openBootcamp/tree/main/Python/mathy
 
 print(mat.suma(5,3))
 print(mat.resta(10,8))
