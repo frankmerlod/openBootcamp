08866e7501315c35ce062d44618ebe85aa7425fe
Frank Merlo <86177938+frankmerlod@users.noreply.github.com>
2023-05-02 02:50:13 -0300

    Update modulo_mathy.py

--- Python/modulo_mathy.py
+++ Python/modulo_mathy.py
@@ -4,3 +4,6 @@ print(mat.suma(5,3))
 print(mat.resta(10,8))
 print(mat.multiplicacion(7,3))
 print(mat.division(30,4))
+
+
+# Modulo math ==> https://github.com/frankmerlod/openBootcamp/tree/main/Python/mathy
